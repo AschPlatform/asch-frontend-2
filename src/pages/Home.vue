0784d092ba7209bc0bd141a210eb9e874233e531
dujunhui007 <810743626@qq.com>
2018-04-17 16:38:27 +0800

    change home style

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,63 +2,82 @@
   <!-- if you want automatic padding use "layout-padding" class -->
   <div v-if="user" class="layout-padding self-center">
     <div class="row gutter-xs col-12">
-      <div class="col-12 col-auto  col-xs-12">
-        <q-card>
-          <q-card-main class="row">
+      <div class="home-top col-12 col-auto  col-xs-12">
+        <q-card class="bg-white no-shadow">
+          <q-card-main class="row shadow-2">
             <div class="col-6">
-              <jdenticon  :address="user.account.address" :size="60" />
+              <jdenticon class="home-jdenticon" :address="user.account.address" :size="60" />
             </div>
-            <div  class="col-6">
-              <q-btn size="xs" :label="$t('TRS_TYPE_TRANSFER')" flat  @click="$root.$emit('openTransactionDialog',{currency:'XAS',precision:8})" />
-              <q-btn size="xs" :label="$t('RECEIVE')"  flat   @click="showAddrQr" />
+            <div class="col-6 row justify-end items-center">
+  
+              <div class="home-top-btn-container">
+                <i class="material-icons font-24 vertical-align-middle text-eight">fiber_manual_record</i>
+                <q-btn class="text-secondary font-24" size="xs" :label="$t('TRS_TYPE_TRANSFER')" flat @click="$root.$emit('openTransactionDialog',{currency:'XAS',precision:8})" />
+              </div>
+              <span class="btn-container-line"></span>
+              <div class="home-top-btn-container">
+                <i class="material-icons font-24 vertical-align-middle text-eight">fiber_manual_record</i>
+                <q-btn class="text-secondary font-24" size="xs" :label="$t('RECEIVE')" flat @click="showAddrQr" />
+              </div>
+  
             </div>
           </q-card-main>
         </q-card>
       </div>
     </div>
   
-    <div class="row col shadow-1 ">
-      <div class="col-5 balance-panel" >
-        <q-card >
+    <div class="home-bottom bg-white no-border row col shadow-1 ">
+      <div class="home-bottom-left col-4 balance-panel">
+        <q-card class="no-shadow">
           <q-card-title>
-            {{$t('BALANCE')}}
+            <i class="material-icons font-24 vertical-align-middle text-secondary">email</i>
+            <span class="text-black font-22">
+             {{$t('BALANCE')}}
+            </span>
+            <span class="text-three font-16">
+              {{$t('MAIN_ASSET')}}
+           </span>
+           <span class="qr-right-container" @click="showAddrQr">
+              <vue-qr class="add-qr-container" :size="20" :text="address"></vue-qr>
+           </span>
           </q-card-title>
-           <q-card-main >
-             {{userInfo.account.xas | fee}}
-             {{$t('MAIN_ASSET')+'XAS'}}
-             <div>  
-             {{address}}
-              <q-btn v-clipboard="address || 'no data'" @success="info('copy senderId success...')" size="xs"  flat round icon="compare arrows" />
-             </div>
-             <div class="row justify-center" @click="showAddrQr">
-               <vue-qr :size="100" :text="address"></vue-qr>
-             </div>
-           </q-card-main>
-          <q-card-main >
-            <q-list v-if="balances.length" highlight>
-              <q-item v-for="(balance,idx) in  balances" :key="idx">
+          <q-card-main>
+            <span class="text-secondary font-30">
+              {{userInfo.account.xas | fee}}
+              </span>
+            <span class="text-secondary font-16">XAS</span>
+  
+            <div class="text-three font-14">
+              {{address}}
+              <q-btn class="text-secondary font-12" v-clipboard="address || 'no data'" @success="info('copy senderId success...')" size="xs" flat round icon="content copy" />
+            </div>
+  
+          </q-card-main>
+          <q-card-main>
+            <q-list class="no-border" v-if="balances.length" highlight>
+              <q-item class="blances-container shadow-1 bg-white" v-for="(balance,idx) in  balances" :key="idx">
                 <q-item-side>
-                  <q-item-tile avatar>
-                    <img :src="null">
+                  <q-item-tile>
+                    <i class="material-icons font-44 vertical-align-middle text-eight">fiber_manual_record</i>
                   </q-item-tile>
                 </q-item-side>
-                <q-item-main :label="balance.currency" />
+                <q-item-main class="text-five font-18" :label="balance.currency" />
                 <q-item-side right>
-                  <q-item-tile >
+                  <q-item-tile class="text-five font-18">
                     {{balance.balance | fee(balance.precision)}}
                   </q-item-tile>
                 </q-item-side>
               </q-item>
             </q-list>
-            <q-btn @click="assets" flat :label="$t('SEE_ALL_ASSETS')" icon="compare arrows" />
+            <q-btn class="home-all-btn q-hoverable bg-secondary text-white q-btn-rounded" color="secondary" @click="assets" flat :label="$t('SEE_ALL_ASSETS')" />
           </q-card-main>
         </q-card>
       </div>
-        <div class="col-7">
-          <trans-record-container  :userInfo="userInfo" />
-        </div>
+      <div class="col-8">
+        <trans-record-container :userInfo="userInfo" />
+      </div>
     </div>
-    
+  
   </div>
 </template>
 
@@ -139,7 +158,66 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.layout-padding {
+  padding: 20px !important;
+}
+
 .balance-panel {
   min-height: 200px;
+}
+
+.home-top {
+  margin-bottom: 20px;
+}
+
+.home-jdenticon {
+  border-radius: 50%;
+}
+
+.home-top-btn-container:hover i {
+  color: #43aea8 !important;
+}
+
+.home-top-btn-container {
+  display: inline-block;
+}
+
+.btn-container-line {
+  margin: 0 20px;
+  display: inline-block;
+  width: 1px;
+  height: 30px;
+  background: #ccc;
+}
+
+.home-bottom-left {
+  padding: 20px;
+}
+
+.qr-right-container {
+  float: right;
+  cursor: pointer;
+}
+
+.add-qr-container {
+  display: inline-block;
+  vertical-align: text-top !important;
+}
+
+.blances-container:hover {
+  background: #ebebeb;
+}
+
+.blances-container {
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+
+.home-all-btn {
+  float: right;
+}
+
+.q-btn {
+  min-height: 0 !important;
 }
 </style>
