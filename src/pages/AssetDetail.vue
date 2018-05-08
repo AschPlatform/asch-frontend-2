ed195dd69999a3eedd0afbf3d137e932a16a11f5
dujunhui007 <810743626@qq.com>
2018-05-08 11:21:35 +0800

    asset of assetDetail add back btn

--- src/pages/AssetDetail.vue
+++ src/pages/AssetDetail.vue
@@ -2,8 +2,15 @@
   <q-page >
     <q-card class="no-shadow">
       <q-card-title>
-      <i class="material-icons vertical-align-middle text-secondary font-22">person</i>
-      <span class="vertical-align-middle font-22">{{$t('ASSET_DETAIL',{currency:asset.currency})}}</span>
+        <div class="row justify-between">
+          <div>
+            <i class="material-icons vertical-align-middle text-secondary font-22">person</i>
+            <span class="vertical-align-middle font-22">{{$t('ASSET_DETAIL',{currency:asset.currency})}}</span>
+          </div>
+          <q-btn color="secondary" icon="reply" class="row items-center" @click="back">
+          </q-btn>
+        </div>
+     
       </q-card-title>
       <q-card-main class="row col-12">
         <assets-panel class="margin-l-15 col-auto" v-if="!isCross" type='inner' :asset="asset" @transfer="transfer"  />
@@ -164,6 +171,9 @@ export default {
       let res = await this.getMoreAssets()
       this.close()
       return res
+    },
+    back() {
+      this.$router.back()
     },
     close() {
       this.$emit('close')
