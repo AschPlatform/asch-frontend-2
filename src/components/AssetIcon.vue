c18417a8ac9469f046df745f8f5e3e28b0f5d6be
dujunhui007 <810743626@qq.com>
2018-06-07 16:25:57 +0800

    change assetIcon of icons name

--- src/components/AssetIcon.vue
+++ src/components/AssetIcon.vue
@@ -2,16 +2,16 @@
     <img :src="assetIcon"  :width="iconSize" :height="iconSize" alt="asset icon">
 </template>
 <script>
-import assetIcon1 from '../assets/asset_icon1.png'
-import assetIcon2 from '../assets/asset_icon2.png'
-import assetIcon3 from '../assets/asset_icon3.png'
-import assetIcon4 from '../assets/asset_icon4.png'
-import assetIcon5 from '../assets/asset_icon5.png'
-import assetIcon6 from '../assets/asset_icon6.png'
-import assetIcon7 from '../assets/asset_icon7.png'
-import assetIcon8 from '../assets/asset_icon8.png'
-import assetIcon9 from '../assets/asset_icon9.png'
-import assetIcon10 from '../assets/asset_icon10.png'
+import xas from '../assets/xas.png'
+import xasChainAssets from '../assets/xas_chain_assets.png'
+import withdraw from '../assets/withdraw.png'
+import transfer from '../assets/transfer.png'
+import receipt from '../assets/receipt.png'
+import pay from '../assets/pay.png'
+import fee from '../assets/fee.png'
+import bch from '../assets/bch.png'
+import btc from '../assets/btc.png'
+import chainTransfer from '../assets/chain_transfer.png'
 
 export default {
   name: 'AssetIcon',
@@ -22,17 +22,16 @@ export default {
 
   data() {
     return {
-      assetIcon1,
-      assetIcon2,
-      assetIcon3,
-      assetIcon4,
-      assetIcon5,
-      assetIcon6,
-      assetIcon7,
-      assetIcon8,
-      assetIcon9,
-      assetIcon10,
-      key: 2
+      xas,
+      xasChainAssets,
+      withdraw,
+      transfer,
+      receipt,
+      pay,
+      fee,
+      bch,
+      btc,
+      chainTransfer
     }
   },
 
@@ -40,20 +39,19 @@ export default {
 
   computed: {
     assetIcon() {
-      let iconArray = [
-        assetIcon1,
-        assetIcon2,
-        assetIcon3,
-        assetIcon4,
-        assetIcon5,
-        assetIcon6,
-        assetIcon7,
-        assetIcon8,
-        assetIcon9,
-        assetIcon10
-      ]
-      // this.key = this.iconKey + 1
-      return iconArray[this.key]
+      let icons = {
+        'XAS': xas,
+        'WITHINCHAINASSET': xasChainAssets,
+        'WITHDRAW': withdraw,
+        'TRANSFER': transfer,
+        'RECEIPT': receipt,
+        'PAY': pay,
+        'FEE': fee,
+        'BCH': bch,
+        'BTC': btc,
+        'CHAINTRANSFER': chainTransfer
+      }
+      return icons.XAS
     },
     iconSize() {
       return this.size ? this.size : '20'
