9e6de9f4b221238557858b2af39de0840bcee824
lbgod2222 <1097843969@qq.com>
2018-08-24 14:37:20 +0800

    :anchor: Unmeant commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -85,7 +85,7 @@ const officialPeers = [
 const urls = {
   server: {
     // development: 'http://123.206.19.30:4096',
-    development: 'http://mainnet.asch.cn:80',
+    development: 'http://47.93.138.148:8192',
     // development: 'http://47.94.37.201:4006',
     // development: 'http://39.106.250.196:4096',
     // development: 'http://39.106.182.193:8192'
