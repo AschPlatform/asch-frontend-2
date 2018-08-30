614ca2257c5acd4abe17655997dad4e1d041c10d
mrcaos <mrcaos@foxmail.com>
2018-08-30 18:21:59 +0800

    feat(#281) fix some issues and feature of asch-general #281

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -83,9 +83,9 @@ const officialPeers = [
 ]
 
 const urls = {
-  serverUrl: 'http://localhost:4096',
+  serverUrl: 'http://mainnet.asch.cn',
   // 区分 local 与 mainnet 的请求头参数
-  magic: '594fe0f3',
+  magic: '5f5b3cf5',
   // 登录接口
   loginApi: {
     mock: '/data/home/accounts.json',
