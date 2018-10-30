35e6d76d39e4ba677ad4b1962cebfb45c65e6317
mrcaos <mrcaos@foxmail.com>
2018-10-30 18:40:00 +0800

    chore(i18n): add i18n

--- src/i18n/zh/index.js
+++ src/i18n/zh/index.js
@@ -961,7 +961,9 @@ const lang = {
   NOT_OPEN: '暂未开通',
   GATEWAY_ALREADY_FREEZED: '网关已冻结',
   CONTRACT_GAS_LIMIT_TIP: '最大不能超过 {num} BCH',
-  BLOCK_ID: '区块ID'
+  BLOCK_ID: '区块ID',
+  ERR_BANCOR_BUY_MORE: '买入额超过 Bancor 限制',
+  ERR_BANCOR_SELL_MORE: '卖出额超过 Bancor 限制'
 }
 
 export default lang
